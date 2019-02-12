396b37bef6d3115d5174e9c5cd80bae96efaba94
jbone187 <jasen.green@prontonmail.com>
2019-02-12 16:47:48 -0700

    Update

---
+++ app.js
@@ -0,0 +1,31 @@
+let hue = require("node-hue-api"),
+  HueApi = hue.HueApi,
+  lightState = hue.lightState;
+
+let displayResult = function(result) {
+  console.log(result);
+};
+
+let displayError = function(err) {
+  console.error(err);
+};
+
+let host = "10.0.0.115",
+  username = "Yh9QU2CRGN76hIqgHYsqUAXkYK5ygh8wfpSEK37H",
+  api = new HueApi(host, username),
+  state = lightState.create();
+
+// Now turn on the lamp
+setTimeout(function() {
+  state.colorLoop();
+
+  api.setLightState(4, state.on(), function(err, result) {
+    if (err) throw err;
+    displayResult(result);
+  });
+
+  api.setLightState(6, state.on(), function(err, result) {
+    if (err) throw err;
+    displayResult(result);
+  });
+}, 1000);
