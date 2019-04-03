dae5ccebd37d56f5940d92c85496b5ec74e972ca
jbone187 <jasen.green@prontonmail.com>
2019-04-03 07:35:11 -0700

    April 3 2019 Update

---
+++ start.js
@@ -0,0 +1,52 @@
+let hue = require("node-hue-api"),
+  HueApi = hue.HueApi,
+  lightState = hue.lightState;
+
+let displayResult = function(result) {
+  console.log(result);
+};
+
+let displayError = function(err) {
+  console.error(err);
+};
+
+let host = "10.0.0.115",
+  username = "Yh9QU2CRGN76hIqgHYsqUAXkYK5ygh8wfpSEK37H",
+  api = new HueApi(host, username),
+  state = lightState.create();
+
+// Now turn on the lamp
+
+function* run() {
+  yield state.rgb(0, 0, 153);
+
+  api.setLightState(4, state.on(), function(err, result) {
+    if (err) throw err;
+    displayResult(result);
+  });
+
+  api.setLightState(6, state.on(), function(err, result) {
+    if (err) throw err;
+    displayResult(result);
+  });
+
+  yield setTimeout(function() {
+    state.rgb(206, 0, 0);
+
+    api.setLightState(4, state.on(), function(err, result) {
+      if (err) throw err;
+      displayResult(result);
+    });
+
+    api.setLightState(6, state.on(), function(err, result) {
+      if (err) throw err;
+      displayResult(result);
+    });
+  }, 5000);
+}
+
+setInterval(function() {
+  let partylight = run();
+  partylight.next();
+  partylight.next();
+}, 10000);
--- stop.js
+++ stop.js
@@ -11,20 +11,17 @@ let displayError = function(err) {
 };
 
 let host = "10.0.0.115",
-  username = "",
+  username = "Yh9QU2CRGN76hIqgHYsqUAXkYK5ygh8wfpSEK37H",
   api = new HueApi(host, username),
   state = lightState.create();
 
-// Now turn on the lamp
 setTimeout(function() {
-  state.colorLoop();
-
-  api.setLightState(4, state.on(), function(err, result) {
+  api.setLightState(4, state.off(), function(err, result) {
     if (err) throw err;
     displayResult(result);
   });
 
-  api.setLightState(6, state.on(), function(err, result) {
+  api.setLightState(6, state.off(), function(err, result) {
     if (err) throw err;
     displayResult(result);
   });
