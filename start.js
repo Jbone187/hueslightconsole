fb692a1e921ea01c794874143752bef4318ab758
jbone187 <jasen.green@prontonmail.com>
2019-04-23 21:09:35 -0700

    Update

--- start.js
+++ start.js
@@ -2,13 +2,13 @@ let hue = require("node-hue-api"),
   HueApi = hue.HueApi,
   lightState = hue.lightState;
 
-let inquirer = require('inquirer');
+let inquirer = require("inquirer");
 
-let displayResult = function (result) {
+let displayResult = function(result) {
   console.log(result);
 };
 
-let displayError = function (err) {
+let displayError = function(err) {
   console.error(err);
 };
 
@@ -19,80 +19,72 @@ let host = "10.0.0.115",
 
 // Now turn on the lamp
 
-
 inquirer
-  .prompt([{
-      type: 'list',
-      name: 'BC1',
+  .prompt([
+    {
+      type: "list",
+      name: "BC1",
       message: "Please select first bulb color",
-      choices: ['Red', 'Blue', 'Green', 'Purple']
+      choices: ["Red", "Blue", "Green", "Purple"]
     },
     {
-      type: 'list',
-      name: 'BC2',
+      type: "list",
+      name: "BC2",
       message: "Please select second bulb color",
-      choices: ['Red', 'Blue', 'Green', 'Purple']
-
+      choices: ["Red", "Blue", "Green", "Purple"]
     }
   ])
   .then(answers => {
-    console.log(`You have selected ${answers.BC1} and ${answers.BC2} as your bulb color's`);
-
-    let Blue = state.rgb(0, 0, 255);
-    let Red = state.rgb(255, 0, 0);
-    let Green = state.rgb(0, 204, 0);
-    let Purple = state.rgb(153, 0, 153);
-
+    console.log(
+      `You have selected ${answers.BC1} and ${answers.BC2} as your bulb color's`
+    );
 
     function setOne() {
-      // state.rgb(0, 0, 255);
       function answersBC1(color) {
-        if (color === 'Red') {
-          Red()
-        } else if (color === 'Blue') {
-          Blue()
-        } else if (color === 'Green') {
-          Green()
-        } else if (color === 'Purple') {
-          Purple()
+        if (color === "Red") {
+          state.rgb(255, 0, 0);
+        } else if (color === "Blue") {
+          state.rgb(0, 0, 255);
+        } else if (color === "Green") {
+          state.rgb(23, 88, 7);
+        } else if (color === "Purple") {
+          state.rgb(160, 32, 240);
         }
-      };
-      answersBC1(answers.BC1)
+      }
+      answersBC1(answers.BC1);
 
-      api.setLightState(4, state.on(), function (err, result) {
+      api.setLightState(4, state.on(), function(err, result) {
         if (err) throw err;
         displayResult(result);
       });
 
-      api.setLightState(6, state.on(), function (err, result) {
+      api.setLightState(6, state.on(), function(err, result) {
         if (err) throw err;
         displayResult(result);
       });
     }
 
     function setTwo() {
-      setTimeout(function () {
-        // state.rgb(255, 0, 0);
-
-        function answersBC2(color) {
-          if (color === 'Red') {
-            Red()
-          } else if (color === 'Blue') {
-            Blue()
-          } else if (color === 'Green') {
-            Green()
-          } else if (color === 'Purple') {
-            Purple()
+      setTimeout(function() {
+        function answersBC1(color) {
+          if (color === "Red") {
+            state.rgb(255, 0, 0);
+          } else if (color === "Blue") {
+            state.rgb(0, 0, 255);
+          } else if (color === "Green") {
+            state.rgb(23, 88, 7);
+          } else if (color === "Purple") {
+            state.rgb(160, 32, 240);
           }
-        };
-        answersBC2(answers.BC2)
+        }
+        answersBC1(answers.BC2);
 
-        api.setLightState(4, state.on(), function (err, result) {
+        api.setLightState(4, state.on(), function(err, result) {
           if (err) throw err;
           displayResult(result);
         });
 
-        api.setLightState(6, state.on(), function (err, result) {
+        api.setLightState(6, state.on(), function(err, result) {
           if (err) throw err;
           displayResult(result);
         });
@@ -105,10 +97,9 @@ inquirer
       yield setTwo();
     }
 
-    setInterval(function () {
+    setInterval(function() {
       let partylight = run();
       partylight.next();
       partylight.next();
     }, 10000);
-
   });
